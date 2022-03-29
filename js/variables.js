b8978807bd1c6f4b1609eef94f59e31fbc7f4543
TomasUri <urinovic.tomas@gmail.com>
2022-03-29 13:22:09 +0300

    Practice

---
+++ js/variables.js
@@ -0,0 +1,45 @@
+/*
+
+KINTAMIEJI:
+
+Iniciavimo budas:
+const - default inicijavimo budas
+let - antrinis budas inicijuoti, kai zimone, jog reiksme tures keistis
+var - pats seniausias ir geriau niekada nenaudoti, jei rupi saugumas
+
+Duomenu tipas:
+- number (skaicius)
+- string (tekstas)
+- boolean (logine reiksme: true | false)
+- array (masyvas, sarasas, matrica*)
+
+*/
+const luckyNumber = 7;
+console.log(luckyNumber);
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 5;
+console.log(pinigine);
+const vardas = 'Maryte';
+const pasisveikinimas = 'Laba diena, ' + vardas + '.'
+console.log(pasisveikinimas);
+
+const arVedes = true;
+const arTuriVaiku = false;
+const arMeluojaDelVaiku = true;
+console.log('Ar vedes?', arVedes);
+
+
+console.log(1, 2, 3);
+
+const pazymiai = [10, 2, 4, 8, 6];
+console.log('Pazymiai', pazymiai);
+
+const studentuVardai = ['Petras', 'Jonas'];
+console.log('Studentai', studentuVardai);
+
+const booleanList = [true, false, true, false]
+console.log(booleanList);
+
+const random = [1, 'a', true, []];
