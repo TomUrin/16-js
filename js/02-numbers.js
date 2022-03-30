6ccf30b0466bd66347636220aec95a1e28a15806
TomasUri <urinovic.tomas@gmail.com>
2022-03-30 11:09:42 +0300

    Changes

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -6,6 +6,11 @@ NUMBERS:
 - nenormalus:
     - NaN (not-a-number)
     - Infinity; -Infinity
+
+OPERATORIAI:
+++ - padidinimas vienu vienetu
+-- - sumazinimas vienu vienetu
++=, -=, *=, /= - savo paties reiksmes pakeitimas (a = a + 5 --> a += 5)
 */
 
 const sveikasis = 5;
@@ -14,3 +19,61 @@ const neigiamasDesimtainis = -3.14;
 const neSkaicius = NaN;
 const begalybe = Infinity;
 const neigiamaBegalybe = -Infinity;
+
+console.log('-----------');
+console.log(2 ** 4);
+console.log(3.14 ** 3.14);
+console.log(7 % 2);
+console.log(13 % 5);
+
+console.log('-----------');
+let pinigineAfter = 0;
+console.log('Pinigine:', pinigineAfter++);
+console.log('Pinigine:', pinigineAfter++);
+
+console.log('-----------');
+let pinigineBefore = 0;
+console.log('Pinigine:', ++pinigineBefore);
+console.log('Pinigine:', ++pinigineBefore);
+
+console.log('-----------');
+let skolaAfter = 0;
+console.log('skola:', skolaAfter--);
+console.log('skola:', skolaAfter--);
+
+console.log('-----------');
+let skolaBefore = 0;
+console.log('skola:', --skolaBefore);
+console.log('skola:', --skolaBefore);
+
+let a = 0;
+a = a +1;
+console.log(a);
+a++
+console.log(a);
+
+console.log('-----------');
+let b = 0;
+b = b + 3;
+console.log(b);
+b = b + 3;
+console.log(b);
+b = b + 3;
+console.log(b);
+b += 3;
+console.log(b);
+
+console.log('-----------');
+let c = 16;
+const d = 2;
+console.log(c);
+c = c / d;
+console.log(c);
+c /= d;
+console.log(c);
+
+console.log('-----------');
+let e = 5;
+console.log(e);
+e /= e;
+console.log(e);
