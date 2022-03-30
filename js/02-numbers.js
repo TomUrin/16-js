35cf0567a6f88c30888d23a44f662ff621ad26da
TomasUri <urinovic.tomas@gmail.com>
2022-03-30 10:36:24 +0300

    Initial commit

---
+++ js/02-numbers.js
@@ -0,0 +1,16 @@
+/*
+NUMBERS:
+- normalus:
+    - sveikieji
+    - desimtainiai
+- nenormalus:
+    - NaN (not-a-number)
+    - Infinity; -Infinity
+*/
+
+const sveikasis = 5;
+const desimtainis = 3.14;
+const neigiamasDesimtainis = -3.14;
+const neSkaicius = NaN;
+const begalybe = Infinity;
+const neigiamaBegalybe = -Infinity;
