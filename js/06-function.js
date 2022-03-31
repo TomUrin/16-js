b19617a99eaa376f4fb0786c72430e752cc134ba
TomasUri <urinovic.tomas@gmail.com>
2022-03-31 11:44:15 +0300

    06-function

--- js/06-function.js
+++ js/06-function.js
@@ -49,5 +49,48 @@ const onosVidurkis = vidurkis(ona);
 console.log('Ona:', onosVidurkis);
 
 /*
-Funkcija 
+Funkcija gauna varda ir turi grazinti sakini:
+"Vardo pirma raide yra [V] ir vardas sudarytas is [5] raidziu."
+*/
+
+function vardoAnalize(name) {
+    return `Vardo "${name}" pirma raide yra ${name[0]} ir vardas sudarytas is ${name.length} raidziu.`
+}
+
+const petroVardas = vardoAnalize('Petras');
+console.log(petroVardas);
+
+/*
+Funkcija gauna du skaicius ir turi grazinti su sprendimu, pvz.:
+2 + 2 = 4
+*/
+
+function suma(a, b) {
+    return `${a} + ${b} = ${a + b}`;
+}
+
+const s1 = suma(2, 3);
+console.log(s1);
+
+const s2 = suma(0.1, 0.2);
+console.log(s2);
+
+const s3 = suma(-88, -11);
+console.log(s3);
+
+/*
+desimtainis dvejetainis 
+0           000
+1           001
+2           010
+3           011
+4           100
+
+
+0.5         5 + 10^-1      [atmintyje laiko: sveika, desimtaine, laipsni]
+0.55        5,5 + 10^-1
+
+0.07        7 * 10^-2
+0.0714      7,14 * 10^-2    [7, 14, -2]
+
 */
