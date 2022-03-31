a153384e57734b32e45d186fb879535a1c41da63
TomasUri <urinovic.tomas@gmail.com>
2022-03-31 12:38:05 +0300

    if

--- js/05-if.js
+++ js/05-if.js
@@ -4,6 +4,7 @@ IF
 Sablonai:
 - if () {}
 - if () {} else () {}
+- if () {} else if () {}
 
 
 Palyginimo operatoriai:
@@ -21,3 +22,45 @@ if (a === b) {
     console.log('A nera lygu B');
 }
 
+
+
+const day = 1;
+if (day === 1) {
+    console.log('pirmadienis');
+} else if (day === 2) {
+    console.log('antradienis');
+}
+
+console.log('-------------------');
+
+const akiuSpalva = 'melyna';
+
+if (akiuSpalva === 'zalia') {
+    console.log('Zaliaakiai yra zalciai!');
+} else {
+    if (akiuSpalva === 'melyna') {
+        console.log('Melynakiai megsta melynes.');
+    }
+    
+}
+
+console.log('-------------------');
+
+const temperatura = -25;
+const nuoKadaLaikomeJogSilta = 15;
+const arYraKrituliu = true;
+
+
+if (arYraKrituliu === true) {
+    if (temperatura >= nuoKadaLaikomeJogSilta) {
+        console.log('Varyk su maike');
+    } else {
+        console.log('Tau reikes kailiniu');
+    }
+} else {
+    if (temperatura >= nuoKadaLaikomeJogSilta) {
+        console.log('Gali eiti nuogas');
+    } else {
+        console.log('Be striukes neapsieisi');
+    }
+}
